067e6d73d61965321f4fff70897d745f876acca3
stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-07-22 23:33:43 +0300

    App theme

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+import { AuthContext, AuthProvider } from "./contexts/AuthContext";
+import useAuth from "./hooks/useAuth";
+import defaultTheme from "./themes/default-theme";
+
+export {
+  AuthContext,
+  AuthProvider,
+  useAuth,
+  defaultTheme,
+};
