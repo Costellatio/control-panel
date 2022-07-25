ffc9900c374cb013cdb16b0ea652eb8a4e19f296
stuksi <stoyan.konstantinov.grozdanov@gmail.com>
2022-07-25 23:35:56 +0300

    Panel component structure, empty analytics

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,8 +1,31 @@
-import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
+import { RealTimeProvider } from "../lib";
+import Analytics from "./Analytics";
+import Controller from "./Controller";
+import Visualization from "./Visualization";
 
 const Panel = () => {
   return (
-    <Typography color={'white'}>WIP</Typography>
+    <Grid
+      container
+      width='100%'
+      height='100%'
+      direction='column'
+      justifyContent='center'
+      alignItems='center'
+      spacing={2}
+      sx={{
+        '& .MuiGrid-item': {
+          width: '50%',
+        }
+      }}
+    >
+      <RealTimeProvider>
+        <Grid item xs={12}><Controller /></Grid>
+        <Grid item xs={10}><Visualization /></Grid>
+      </RealTimeProvider>
+      <Grid item xs={2}><Analytics /></Grid>
+    </Grid>
   );
 }
 
